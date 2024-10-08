c7f7104b3937d30260056f88e73c8ce2445ba538
williamqsnt <william.quesnot@ecoles-epsi.net>
2024-10-08 15:21:04 +0200

    Ajout de style

--- Hygie.Back/Services/QuizService.cs
+++ Hygie.Back/Services/QuizService.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using Hygie.Model;
 
 namespace Hygie.Back.Services
@@ -14,9 +10,22 @@ namespace Hygie.Back.Services
 
         public Contenu GetContenu()
         {
-            string pathJson = "..\\Hygie.Back\\Data\\questions.json";
-            string json = System.IO.File.ReadAllText(pathJson);
+            string pathJson;
 
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                pathJson = "..\\Hygie.Back\\Data\\questions.json"; // Chemin pour Windows
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                pathJson = "../Hygie.Back/Data/questions.json"; // Chemin pour macOS
+            }
+            else
+            {
+                throw new PlatformNotSupportedException("Unsupported platform.");
+            }
+
+            string json = System.IO.File.ReadAllText(pathJson);
             Contenu contenu = System.Text.Json.JsonSerializer.Deserialize<Contenu>(json);
 
             return contenu;
