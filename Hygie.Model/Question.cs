484a2214ebf4cef97367c9b8cb229b6cd9d1b3a3
Maxime <98897255+Blamax23@users.noreply.github.com>
2024-10-08 15:53:34 +0200

    Ajout score question

--- Hygie.Model/Question.cs
+++ Hygie.Model/Question.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Hygie.Model
@@ -12,7 +13,23 @@ namespace Hygie.Model
 
         public string Intitule { get; set; }
 
+        [JsonPropertyName("ScoreTotal")]
+        public int Score { get; set; }
+
         public List<Reponse> Reponses { get; set; }
+
+        public void RecalculateScore()
+        {
+            var indice = 6 / 10;
+            if(this.Reponses.Count > 0)
+            {
+                foreach (var rep in this.Reponses)
+                {
+                    if (rep.Score == 10)
+                        rep.Score = rep.Score * indice;
+                }
+            }
+        }
 
     }
 }
